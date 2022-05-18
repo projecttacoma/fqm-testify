6cebb72fa0720081b5007fc7f502f823c83bddb1
natjoe4 <njjones@mitre.org>
2022-05-18 16:41:06 -0400

    fixed types issue

--- __tests__/components/MeasureUpload.test.tsx
+++ __tests__/components/MeasureUpload.test.tsx
@@ -14,7 +14,7 @@ describe('MeasureUpload', () => {
     expect(title).toBeInTheDocument();
   });
   it('renders a dropzone with measure bundle name label when measure uploaded', () => {
-    const MockMB = getMockRecoilState(measureBundleState, { name: 'testName' });
+    const MockMB = getMockRecoilState(measureBundleState, new File([], 'testName'));
     render(
       mantineRecoilWrap(
         <>
--- state/atoms/measureBundle.ts
+++ state/atoms/measureBundle.ts
@@ -3,7 +3,7 @@ import { atom } from 'recoil';
 /**
  * Atom tracking and controlling the value of uploaded measure bundle
  */
-export const measureBundleState = atom({
+export const measureBundleState = atom<File | null>({
   key: 'measureBundleState',
   default: null
 });
