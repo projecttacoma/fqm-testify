2ac39010a90fedfa9b7e6f98a2347057a06a11ec
Elsa <eperelli@mitre.org>
2022-07-22 11:48:30 -0400

    Addressed PR comments- UI bug fix

--- components/ConfirmationModal.tsx
+++ components/ConfirmationModal.tsx
@@ -1,4 +1,4 @@
-import { Modal, Button, Center, Group, Text } from '@mantine/core';
+import { Modal, Button, Center, Group, Text, Grid } from '@mantine/core';
 import { AlertTriangle } from 'tabler-icons-react';
 
 export interface ConfirmationModalProps {
@@ -10,23 +10,37 @@ export interface ConfirmationModalProps {
 
 export default function ConfirmationModal({ open = true, onClose, title, onConfirm }: ConfirmationModalProps) {
   return (
-    <Modal data-testid="confirmation-modal" opened={open} onClose={onClose} withCloseButton={false} size="30%">
-      <Center>
-        <AlertTriangle color="red" />
-        <Text size="sm" weight={700}>
-          &nbsp;{title}
-        </Text>
-      </Center>
-      <Center>
-        <Group style={{ paddingTop: '12px' }}>
-          <Button data-testid="yes-button" onClick={() => onConfirm()}>
-            Yes
-          </Button>
-          <Button data-testid="cancel-button" color="gray" onClick={onClose}>
-            Cancel
-          </Button>
-        </Group>
-      </Center>
+    <Modal
+      data-testid="confirmation-modal"
+      zIndex={2}
+      overflow="outside"
+      opened={open}
+      onClose={onClose}
+      withCloseButton={false}
+      size="lg"
+    >
+      <Grid align="center" justify="center">
+        <Grid.Col>
+          <Center>
+            <AlertTriangle color="red" size={35} />
+            <Text size="sm" weight={700} align="center">
+              &nbsp;{title}
+            </Text>
+          </Center>
+        </Grid.Col>
+        <Grid.Col>
+          <Center>
+            <Group style={{ paddingTop: '5px' }}>
+              <Button data-testid="yes-button" onClick={() => onConfirm()}>
+                Yes
+              </Button>
+              <Button data-testid="cancel-button" color="gray" onClick={onClose}>
+                Cancel
+              </Button>
+            </Group>
+          </Center>
+        </Grid.Col>
+      </Grid>
     </Modal>
   );
 }
