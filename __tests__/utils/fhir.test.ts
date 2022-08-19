955e006db318dd4f24c9eabad2ae89f12507b21f
natjoe4 <35576465+natjoe4@users.noreply.github.com>
2022-08-19 15:58:11 -0400

    Add date paths to test resources (#29)
    * generating primaryDatePath file
    
    * working date fillin
    
    * code is clean
    
    * finished unit tests
    
    * cleaned
    
    * added package.json script
    
    * reran prettier
    
    * added mp to dr run
    
    Co-authored-by: natjoe4 <njjones@mitre.org>

--- __tests__/utils/fhir.test.ts
+++ __tests__/utils/fhir.test.ts
@@ -1,9 +1,28 @@
+import { dateFieldInfo } from '../../scripts/parsePrimaryDatePath';
 import {
   createFHIRResourceString,
   getDataRequirementFiltersString,
   createPatientBundle,
-  getFhirResourceSummary
+  getFhirResourceSummary,
+  getRandomPeriodInPeriod,
+  getRandomDateInPeriod,
+  jsDateToFHIRDate,
+  getDateType,
+  getResourcePrimaryDates
 } from '../../util/fhir';
+import {
+  EXAMPLE_DR_CHOICE_TYPE_PERIOD_TO_PERIOD,
+  EXAMPLE_DR_CHOICE_TYPE_DATETIME_TO_PERIOD,
+  EXAMPLE_DR_PERIOD_TO_DATETIME,
+  EXAMPLE_DR_DATETIME_TO_DATETIME,
+  EXAMPLE_DR_PERIOD_TO_DATE,
+  EXAMPLE_DR_DATETIME_TO_DATE,
+  EXAMPLE_DR_MULTIPLE_DATE_FILTERS,
+  EXAMPLE_DR_NO_DATEFILTER
+} from '../../fixtures/test/dateFilterFixtures';
+
+const PERIOD_START = '2020-01-01T00:00:00.000Z';
+const PERIOD_END = '2020-12-31T00:00:00.000Z';
 
 const VS_MAP = { testvs: 'test vs name' };
 const DATA_REQUIREMENT_WITH_NO_VALUE_SETS = {
@@ -257,6 +276,25 @@ const MEASURE_REPORT_WITH_ID: fhir4.MeasureReport = {
     status: 'additional'
   }
 };
+const EXAMPLE_FIELD_DATE: dateFieldInfo = {
+  dataTypes: ['date']
+};
+
+const EXAMPLE_FIELD_CHOICE_TYPE_PERIOD: dateFieldInfo = {
+  isChoiceType: true,
+  dataTypes: ['Period', 'dateTime', 'date', 'nonsense']
+};
+const EXAMPLE_FIELD_CHOICE_TYPE_DATETIME: dateFieldInfo = {
+  isChoiceType: true,
+  dataTypes: ['dateTime', 'date', 'nonsense']
+};
+
+const EXAMPLE_FIELD_CHOICE_TYPE_DATE: dateFieldInfo = {
+  isChoiceType: true,
+  dataTypes: ['date', 'nonsense']
+};
+
+const EXAMPLE_PATH = 'testPath';
 
 describe('getDataRequirementFiltersString', () => {
   test('returns an empty string for resource with no valuesets', () => {
@@ -308,7 +346,9 @@ describe('createFHIRResourceString', () => {
     const createdResource = createFHIRResourceString(
       OBSERVATION_DATA_REQUIREMENT_WITH_CODE_AND_VALUESET,
       TEST_MEASURE_BUNDLE,
-      'Patient1'
+      'Patient1',
+      PERIOD_START,
+      PERIOD_END
     );
     expect(JSON.parse(createdResource).code).toEqual({
       coding: [
@@ -329,7 +369,9 @@ describe('createFHIRResourceString', () => {
     const createdResource = createFHIRResourceString(
       MESSAGEDEFINITION_DATA_REQUIREMENT,
       TEST_MEASURE_BUNDLE,
-      'Patient1'
+      'Patient1',
+      PERIOD_START,
+      PERIOD_END
     );
     expect(JSON.parse(createdResource).event).toEqual({
       system: 'test-system',
@@ -344,7 +386,9 @@ describe('createFHIRResourceString', () => {
     const createdResource = createFHIRResourceString(
       OPERATIONDEFINITION_DATA_REQUIREMENT,
       TEST_MEASURE_BUNDLE,
-      'Patient1'
+      'Patient1',
+      PERIOD_START,
+      PERIOD_END
     );
     expect(JSON.parse(createdResource).code).toEqual('123');
     expect(JSON.parse(createdResource).subject).toBeUndefined();
@@ -354,7 +398,9 @@ describe('createFHIRResourceString', () => {
     const createdResource = createFHIRResourceString(
       ACTIVITYDEFINITION_DATA_REQUIREMENT,
       TEST_MEASURE_BUNDLE,
-      'Patient1'
+      'Patient1',
+      PERIOD_START,
+      PERIOD_END
     );
     expect(Array.isArray(JSON.parse(createdResource).topic)).toBe(true);
     expect(JSON.parse(createdResource).subject).toBeUndefined();
@@ -364,7 +410,9 @@ describe('createFHIRResourceString', () => {
     const createdResource = createFHIRResourceString(
       OBSERVATION_DATA_REQUIREMENT_WITH_CODE_AND_VALUESET,
       TEST_MEASURE_BUNDLE,
-      'Patient1'
+      'Patient1',
+      PERIOD_START,
+      PERIOD_END
     );
     expect(typeof JSON.parse(createdResource).code).toBe('object');
     expect(JSON.parse(createdResource).subject).toEqual({
@@ -373,7 +421,13 @@ describe('createFHIRResourceString', () => {
   });
 
   test('returns populated FHIR resource where subject is not the patient reference', () => {
-    const createdResource = createFHIRResourceString(COVERAGE_DATA_REQUIREMENT, TEST_MEASURE_BUNDLE, 'Patient1');
+    const createdResource = createFHIRResourceString(
+      COVERAGE_DATA_REQUIREMENT,
+      TEST_MEASURE_BUNDLE,
+      'Patient1',
+      PERIOD_START,
+      PERIOD_END
+    );
     expect(typeof JSON.parse(createdResource).type).toBe('object');
     expect(JSON.parse(createdResource).beneficiary).toEqual({
       reference: 'Patient/Patient1'
@@ -384,7 +438,9 @@ describe('createFHIRResourceString', () => {
     const createdResource = createFHIRResourceString(
       OBSERVATION_DATA_REQUIREMENT_WITH_CODE_AND_VALUESET,
       TEST_MEASURE_BUNDLE_WITH_EXPANSION,
-      'Patient1'
+      'Patient1',
+      PERIOD_START,
+      PERIOD_END
     );
     expect(JSON.parse(createdResource).code).toEqual({
       coding: [
@@ -492,3 +548,144 @@ describe('createPatientBundle', () => {
     expect(createPatientBundle(PATIENT_RESOURCE, [ENCOUNTER_RESOURCE, OBSERVATION_RESOURCE])).toEqual(expectedBundle);
   });
 });
+
+describe('getRandomPeriodInPeriod', () => {
+  beforeAll(() => jest.spyOn(global.Math, 'random').mockReturnValue(0));
+  test('returns a random period within the given period', () => {
+    const period = getRandomPeriodInPeriod(PERIOD_START, PERIOD_END);
+    assertPeriodInPeriod(PERIOD_START, period);
+  });
+  afterAll(jest.clearAllMocks);
+});
+
+describe('getRandomDateInPeriod', () => {
+  beforeAll(() => jest.spyOn(global.Math, 'random').mockReturnValue(0));
+  test('returns a random date within the given period', () => {
+    const date = getRandomDateInPeriod(PERIOD_START, PERIOD_END);
+    expect(date.getTime()).toEqual(new Date(PERIOD_START).getTime());
+    expect(date.getTime()).toBeLessThan(new Date(PERIOD_END).getTime());
+  });
+  afterAll(jest.clearAllMocks);
+});
+
+describe('jsDateToFHIRDate', () => {
+  // TODO figure out how to accurately compare date to dateTime
+  test.skip('converts a JS Date to a FHIR date string', () => {
+    const date = jsDateToFHIRDate(new Date('PERIOD_START'));
+    expect(date).toEqual('2020-01-01');
+  });
+});
+
+describe('getDateType', () => {
+  test('Returns date for date field', () => {
+    const { validField, newPath } = getDateType(EXAMPLE_FIELD_DATE, EXAMPLE_PATH);
+    expect(validField).toEqual('date');
+    expect(newPath).toEqual(EXAMPLE_PATH);
+  });
+  test('Returns Period and updated path for choiceType with Period', () => {
+    const { validField, newPath } = getDateType(EXAMPLE_FIELD_CHOICE_TYPE_PERIOD, EXAMPLE_PATH);
+    expect(validField).toEqual('Period');
+    expect(newPath).toEqual('testPathPeriod');
+  });
+  test('Returns dateTime and updated path for choiceType with dateTime', () => {
+    const { validField, newPath } = getDateType(EXAMPLE_FIELD_CHOICE_TYPE_DATETIME, EXAMPLE_PATH);
+    expect(validField).toEqual('dateTime');
+    expect(newPath).toEqual('testPathDateTime');
+  });
+  test('Returns date and updated path for choiceType with date', () => {
+    const { validField, newPath } = getDateType(EXAMPLE_FIELD_CHOICE_TYPE_DATE, EXAMPLE_PATH);
+    expect(validField).toEqual('date');
+    expect(newPath).toEqual('testPathDate');
+  });
+});
+
+describe('getResourcePrimaryDates', () => {
+  beforeAll(() => jest.spyOn(global.Math, 'random').mockReturnValue(0));
+  test('DataRequirement with singular Period dateFilter on field that accepts Period', () => {
+    const resource: fhir4.Condition = { resourceType: 'Condition', subject: { reference: 'testSubject' } };
+    getResourcePrimaryDates(resource, EXAMPLE_DR_CHOICE_TYPE_PERIOD_TO_PERIOD, PERIOD_START, PERIOD_END);
+    expect(resource.resourceType).toEqual('Condition');
+    assertPeriodInPeriod(EXAMPLE_DR_CHOICE_TYPE_PERIOD_TO_PERIOD.dateFilter[0].valuePeriod.start, resource.onsetPeriod);
+  });
+  test('DataRequirement with singular dateTime dateFilter on field that accepts Period', () => {
+    const resource: fhir4.Condition = { resourceType: 'Condition', subject: { reference: 'testSubject' } };
+    getResourcePrimaryDates(resource, EXAMPLE_DR_CHOICE_TYPE_DATETIME_TO_PERIOD, PERIOD_START, PERIOD_END);
+    expect(resource.resourceType).toEqual('Condition');
+    const start = resource.onsetPeriod?.start;
+    const end = resource.onsetPeriod?.end;
+    if (!start || !end) {
+      throw new Error('resource did not have defined start and end for onsetPeriod');
+    }
+    expect(start).toEqual(EXAMPLE_DR_CHOICE_TYPE_DATETIME_TO_PERIOD.dateFilter[0].valueDateTime);
+    expect(end).toEqual(start);
+  });
+  test('DataRequirement with singular Period dateFilter on field that accepts dateTime', () => {
+    const resource: fhir4.Condition = { resourceType: 'Condition', subject: { reference: 'testSubject' } };
+    getResourcePrimaryDates(resource, EXAMPLE_DR_PERIOD_TO_DATETIME, PERIOD_START, PERIOD_END);
+    expect(resource.resourceType).toEqual('Condition');
+    const dt = resource.recordedDate;
+    if (!dt) {
+      throw new Error('resource did not have defined dateTime for recordedDate');
+    }
+    expect(dt).toEqual(EXAMPLE_DR_PERIOD_TO_DATETIME.dateFilter[0].valuePeriod.start);
+  });
+  test('DataRequirement with singular dateTime dateFilter on field that accepts dateTime', () => {
+    const resource: fhir4.Condition = { resourceType: 'Condition', subject: { reference: 'testSubject' } };
+    getResourcePrimaryDates(resource, EXAMPLE_DR_DATETIME_TO_DATETIME, PERIOD_START, PERIOD_END);
+    expect(resource.resourceType).toEqual('Condition');
+    const dt = resource.recordedDate;
+    expect(dt).toEqual(EXAMPLE_DR_DATETIME_TO_DATETIME.dateFilter[0].valueDateTime);
+  });
+  // TODO figure out how to accurately compare date to dateTime
+  test.skip('DataRequirement with singular Period dateFilter on field that accepts date', () => {
+    const resource: fhir4.Patient = { resourceType: 'Patient' };
+    getResourcePrimaryDates(resource, EXAMPLE_DR_PERIOD_TO_DATE, PERIOD_START, PERIOD_END);
+    expect(resource.resourceType).toEqual('Patient');
+    const dt = resource.birthDate;
+    if (!dt) {
+      throw new Error('resource did not have defined date for birthDate');
+    }
+    const dtTime = new Date(dt).getTime();
+    expect(dtTime).toEqual(new Date(EXAMPLE_DR_PERIOD_TO_DATE.dateFilter[0].valuePeriod.start).getTime());
+  });
+  // TODO figure out how to accurately compare date to dateTime
+  test.skip('DataRequirement with singular dateTime dateFilter on field that accepts date', () => {
+    const resource: fhir4.Patient = { resourceType: 'Patient' };
+    getResourcePrimaryDates(resource, EXAMPLE_DR_DATETIME_TO_DATE, PERIOD_START, PERIOD_END);
+    expect(resource.resourceType).toEqual('Patient');
+    const dt = resource.birthDate;
+    expect(dt).toEqual('2019-01-01');
+  });
+  test('DataRequirement with multiple dateFilters', () => {
+    const resource: fhir4.Condition = { resourceType: 'Condition', subject: { reference: 'testSubject' } };
+    getResourcePrimaryDates(resource, EXAMPLE_DR_MULTIPLE_DATE_FILTERS, PERIOD_START, PERIOD_END);
+    expect(resource.resourceType).toEqual('Condition');
+    assertPeriodInPeriod(
+      EXAMPLE_DR_MULTIPLE_DATE_FILTERS.dateFilter[0].valuePeriod?.start as string,
+      resource.onsetPeriod
+    );
+    const dt = resource.recordedDate;
+    expect(dt).toEqual(EXAMPLE_DR_MULTIPLE_DATE_FILTERS.dateFilter[1].valueDateTime);
+  });
+  test('DataRequirement with no dateFilters', () => {
+    const resource: fhir4.Condition = { resourceType: 'Condition', subject: { reference: 'testSubject' } };
+    getResourcePrimaryDates(resource, EXAMPLE_DR_NO_DATEFILTER, PERIOD_START, PERIOD_END);
+    expect(resource.resourceType).toEqual('Condition');
+    assertPeriodInPeriod(PERIOD_START, resource.onsetPeriod);
+    assertPeriodInPeriod(PERIOD_START, resource.abatementPeriod);
+    const dt = resource.recordedDate;
+    expect(dt).toEqual(PERIOD_START);
+  });
+
+  afterAll(jest.clearAllMocks);
+});
+
+function assertPeriodInPeriod(start: string, period?: fhir4.Period) {
+  if (!period?.start || !period?.end) {
+    throw new Error('resource did not have defined start and end for period');
+  }
+  const startTime = new Date(period.start).getTime();
+  const endTime = new Date(period.end).getTime();
+  expect(period.start).toEqual(start);
+  expect(Math.round((endTime - startTime) / (1000 * 60 * 60 * 24))).toEqual(1);
+}
--- util/fhir.ts
+++ util/fhir.ts
@@ -2,9 +2,13 @@ import { v4 as uuidv4 } from 'uuid';
 import { getRandomFirstName, getRandomLastName } from './randomizer';
 import { ValueSetsMap } from '../state/selectors/valueSetsMap';
 import { parsedPrimaryCodePaths } from './primaryCodePaths';
+import { parsedPrimaryDatePaths } from './primaryDatePaths';
 import _ from 'lodash';
 import { ReferencesMap } from './referencesMap';
 import fhirpath from 'fhirpath';
+import { dateFieldInfo } from '../scripts/parsePrimaryDatePath';
+
+const DEFAULT_PERIOD_LENGTH = 1;
 
 export function createPatientResourceString(birthDate: string): string {
   const id = uuidv4();
@@ -141,13 +145,33 @@ export function createPatientBundle(patient: fhir4.Patient, resources: fhir4.Fhi
 export function createFHIRResourceString(
   dr: fhir4.DataRequirement,
   mb: fhir4.Bundle,
-  patientId: string | null
+  patientId: string | null,
+  mpStart: string,
+  mpEnd: string
 ): string {
   const resource: any = {
     resourceType: dr.type,
     id: uuidv4()
   };
+  getResourcePrimaryCode(resource, dr, mb);
+  getResourcePatientReference(resource, dr, patientId);
+  getResourcePrimaryDates(resource, dr, mpStart, mpEnd);
+  return JSON.stringify(resource, null, 2);
+}
 
+function getResourcePatientReference(resource: any, dr: fhir4.DataRequirement, patientId: string | null) {
+  // determine if we should add a reference to the patient
+  if (ReferencesMap[dr.type] && patientId) {
+    // add if subject is in the list otherwise add it on the first one
+    if (ReferencesMap[dr.type].includes('subject')) {
+      resource.subject = { reference: `Patient/${patientId}` };
+    } else if (ReferencesMap[dr.type][0] && !ReferencesMap[dr.type][0].includes('.')) {
+      resource[ReferencesMap[dr.type][0]] = { reference: `Patient/${patientId}` };
+    }
+  }
+}
+
+function getResourcePrimaryCode(resource: any, dr: fhir4.DataRequirement, mb: fhir4.Bundle) {
   // resource properties retrieved from data requirements
   dr.codeFilter?.forEach(cf => {
     if (!cf.valueSet && cf.path && cf.code) {
@@ -200,16 +224,156 @@ export function createFHIRResourceString(
   }
 
   resource[primaryCodePath] = parsedPrimaryCodePaths[dr.type].multipleCardinality ? [codeData] : codeData;
+}
 
-  // determine if we should add a reference to the patient
-  if (ReferencesMap[dr.type] && patientId) {
-    // add if subject is in the list otherwise add it on the first one
-    if (ReferencesMap[dr.type].includes('subject')) {
-      resource.subject = { reference: `Patient/${patientId}` };
-    } else if (ReferencesMap[dr.type][0] && !ReferencesMap[dr.type][0].includes('.')) {
-      resource[ReferencesMap[dr.type][0]] = { reference: `Patient/${patientId}` };
+/**
+ * Parses the primary date path info and populates date info to satisfy date filter specified in the passed-in data requirement
+ * @param {Object} resource FHIR resource being created
+ * @param {Object} dr a FHIR dataRequirement object
+ * @param {String} mpStart the start of the specified measurement period
+ * @param {String} mpEnd the end of the specified measurement period
+ */
+export function getResourcePrimaryDates(resource: any, dr: fhir4.DataRequirement, mpStart: string, mpEnd: string) {
+  const rt = dr.type;
+  const primaryDateInfo = parsedPrimaryDatePaths[rt];
+  if (primaryDateInfo) {
+    if (dr.dateFilter && dr.dateFilter.length > 0) {
+      dr.dateFilter?.forEach(df => {
+        // pull path off date filter
+        let path = df.path?.split('.')[0] ?? '';
+        // check if path exists on primary date info
+        if (Object.keys(primaryDateInfo).includes(path)) {
+          const fieldTypeInfo = primaryDateInfo[path];
+          // check for allowed types for value with priority (Period => dateTime => date)
+          const { validField, newPath } = getDateType(fieldTypeInfo, path);
+          if (validField === 'Period') {
+            if (df.valuePeriod) {
+              // pick random 1-day period in period
+              resource[newPath] = getRandomPeriodInPeriod(
+                df.valuePeriod?.start ?? mpStart,
+                df.valuePeriod?.end ?? mpEnd
+              );
+            } else if (df.valueDateTime) {
+              // use dateTime as periodStart and periodEnd of resource
+              resource[newPath] = { start: df.valueDateTime, end: df.valueDateTime };
+            } else {
+              // pick random period within the measurement period and make a 1-day period
+              resource[newPath] = getRandomPeriodInPeriod(mpStart, mpEnd);
+            }
+          } else if (validField === 'dateTime') {
+            if (df.valuePeriod) {
+              // pick random dateTime within the period
+              resource[newPath] = getRandomDateInPeriod(
+                df.valuePeriod?.start ?? mpStart,
+                df.valuePeriod?.end ?? mpEnd
+              ).toISOString();
+            } else if (df.valueDateTime) {
+              // use valueDateTime
+              resource[newPath] = df.valueDateTime;
+            } else {
+              // pick random dateTime within the measurement period
+              resource[newPath] = getRandomDateInPeriod(mpStart, mpEnd).toISOString();
+            }
+          } else if (validField === 'date') {
+            if (df.valuePeriod) {
+              // pick random date in period
+              const date = getRandomDateInPeriod(df.valuePeriod?.start ?? mpStart, df.valuePeriod?.end ?? mpEnd);
+              resource[newPath] = jsDateToFHIRDate(date);
+            } else if (df.valueDateTime) {
+              // use valueDateTime and strip timezone
+              resource[newPath] = jsDateToFHIRDate(new Date(df.valueDateTime));
+            } else {
+              //pick random date within the measurement period
+              const date = getRandomDateInPeriod(mpStart, mpEnd);
+              resource[newPath] = jsDateToFHIRDate(date);
+            }
+          }
+        }
+      });
+      // If no date filters, fill all fields that can accept Period, dateTime, or date with valid entries in mp
+    } else {
+      Object.keys(primaryDateInfo).forEach(path => {
+        const fieldTypeInfo = primaryDateInfo[path];
+        const { validField, newPath } = getDateType(fieldTypeInfo, path);
+        if (validField === 'Period') {
+          resource[newPath] = getRandomPeriodInPeriod(mpStart, mpEnd);
+        } else if (validField === 'dateTime') {
+          resource[newPath] = getRandomDateInPeriod(mpStart, mpEnd).toISOString();
+        } else {
+          resource[newPath] = getRandomDateInPeriod(mpStart, mpEnd).toISOString();
+        }
+      });
     }
   }
+}
 
-  return JSON.stringify(resource, null, 2);
+/**
+ * Determines the ideal date info type and path to populate a given attribute with date info
+ * @param {Object} fieldTypeInfo object containing information on date types the specified field can accept
+ * @param {String} path the attribute name that needs to be populated with date info
+ * @returns {Object} an object containing the newPath to enter date info and the ideal date info type
+ */
+export function getDateType(fieldTypeInfo: dateFieldInfo, path: string) {
+  let validField;
+  let newPath = path;
+  if (fieldTypeInfo.isChoiceType) {
+    // prioritize date type Period > dateTime > date
+    validField = fieldTypeInfo.dataTypes.reduce((acc: string, e: string) => {
+      if (acc === 'Period' || e === 'Period') {
+        return 'Period';
+      } else if (e === 'dateTime') {
+        return 'dateTime';
+      }
+      return acc;
+    }, 'date');
+    // for choiceTypes append the capitalized dateType to the field (ex. effectivePeriod)
+    newPath = `${path}${validField.charAt(0).toUpperCase() + validField.slice(1)}`;
+  } else {
+    validField = fieldTypeInfo.dataTypes[0];
+  }
+  return { validField, newPath };
+}
+
+/**
+ * Creates a JS date at a random time within the specified period
+ * @param {String} start start date string for the period
+ * @param {String} end end date string for the period
+ * @returns {Date} a js date object within the specified period
+ */
+export function getRandomDateInPeriod(start: string, end: string) {
+  const startDate = new Date(start);
+  const endDate = new Date(end);
+  endDate.setDate(endDate.getDate() - 1);
+
+  var date = new Date(+startDate + Math.random() * (endDate.getTime() - startDate.getTime()));
+  return date;
+}
+
+/**
+ * Converts a JS date to a FHIR date string
+ * @param {Date} date JS date to be converted
+ * @returns {String} a string representing a FHIR date
+ */
+export function jsDateToFHIRDate(date: Date) {
+  const year = date.getFullYear();
+  // month is 0 indexed
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  return `${year}-${month < 10 ? `0${month}` : month}-${day < 10 ? `0${day}` : day}`;
+}
+
+/**
+ * Takes in the start and end points of a period and returns a FHIR Period that takes place within the specified dates
+ * @param {string} start start date string for the period
+ * @param {string} end end date string for the period
+ * @returns {Object} a FHIR Period with length equal to the default period length that takes place within the passed-in period
+ */
+export function getRandomPeriodInPeriod(start: string, end: string): fhir4.Period {
+  const periodStart = getRandomDateInPeriod(start, end);
+  const periodEnd = new Date(periodStart);
+  periodEnd.setDate(periodEnd.getDate() + DEFAULT_PERIOD_LENGTH);
+  return {
+    start: periodStart.toISOString(),
+    end: periodEnd.toISOString()
+  };
 }
