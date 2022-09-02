383c167417bb7b3615071ee9da2b4f1ded3c0a49
natjoe4 <njjones@mitre.org>
2022-09-02 13:45:08 -0400

    working

--- components/TestCaseEditor.tsx
+++ components/TestCaseEditor.tsx
@@ -1,25 +1,20 @@
-import { Card, createStyles, Grid, Text } from '@mantine/core';
+import { createStyles, Grid, Text } from '@mantine/core';
 import React from 'react';
 import { useRecoilValue } from 'recoil';
 import { selectedPatientState } from '../state/atoms/selectedPatient';
 import PatientCreationPanel from './patient-creation/PatientCreationPanel';
 import ResourcePanel from './resource-creation/ResourcePanel';
 import MeasureHighlightingPanel from './calculation/MeasureHighlightingPanel';
-import { measureBundleState } from '../state/atoms/measureBundle';
-import { measurementPeriodStringState } from '../state/selectors/measurementPeriodString';
 
 const useStyles = createStyles({
   panel: {
     maxHeight: '100%',
-    overflow: 'scroll',
-    flexGrow: 3
+    overflow: 'scroll'
   }
 });
 
 export default function TestCaseEditor() {
   const selectedPatient = useRecoilValue(selectedPatientState);
-  const measurementPeriodString = useRecoilValue(measurementPeriodStringState);
-  const measureBundle = useRecoilValue(measureBundleState);
 
   const { classes } = useStyles();
 
@@ -34,29 +29,6 @@ export default function TestCaseEditor() {
   return (
     <>
       <Grid style={{ height: '70vh' }}>
-        <Grid.Col span={12} sx={theme => ({ backgroundColor: 'black', height: 'fit-content' })}>
-          <div
-            style={{
-              display: 'flex',
-              marginLeft: 15,
-              marginRight: 15,
-              justifyContent: 'space-between',
-              alignItems: 'center'
-            }}
-          >
-            <Card sx={theme => ({ width: 'fit-content' })}>
-              <Text size="md" color="dimmed">
-                {measureBundle.name}
-              </Text>
-              <Text size="sm" color="dimmed">
-                {measurementPeriodString}
-              </Text>
-            </Card>
-            <Text color="dimmed" size="xl">
-              FQM Testify: an eCQM Analysis Tool
-            </Text>
-          </div>
-        </Grid.Col>
         <Grid.Col span={3} className={classes.panel}>
           <PatientCreationPanel />
         </Grid.Col>
--- pages/generateTestCases.tsx
+++ pages/generateTestCases.tsx
@@ -1,9 +1,52 @@
+import { Button, Card, Grid, Text } from '@mantine/core';
+import Link from 'next/link';
+import React from 'react';
+import { useRecoilValue } from 'recoil';
 import TestCaseEditor from '../components/TestCaseEditor';
+import { measureBundleState } from '../state/atoms/measureBundle';
+import { measurementPeriodStringState } from '../state/selectors/measurementPeriodString';
+import { Edit } from 'tabler-icons-react';
 
 export default function TestCaseEditorPage() {
+  const measurementPeriodString = useRecoilValue(measurementPeriodStringState);
+  const measureBundle = useRecoilValue(measureBundleState);
   return (
-    <div style={{ paddingTop: '24px' }}>
-      <TestCaseEditor />
-    </div>
+    <>
+      <Grid>
+        <Grid.Col span={12} sx={() => ({ backgroundColor: 'black', height: 'auto', padding: 20 })}>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center'
+            }}
+          >
+            <Card sx={() => ({ width: 'fit-content' })}>
+              <div style={{ display: 'flex', justifyContent: 'space-around', gap: 10, alignItems: 'center' }}>
+                <div>
+                  <Text size="md" color="dimmed">
+                    {measureBundle.name}
+                  </Text>
+                  <Text size="sm" color="dimmed">
+                    {measurementPeriodString}
+                  </Text>
+                </div>
+                <Button variant="subtle" color="gray">
+                  <Link href={'/'}>
+                    <Edit />
+                  </Link>
+                </Button>
+              </div>
+            </Card>
+            <Text color="dimmed" size="xl">
+              FQM Testify: an eCQM Analysis Tool
+            </Text>
+          </div>
+        </Grid.Col>
+      </Grid>
+      <div style={{ paddingTop: '24px' }}>
+        <TestCaseEditor />
+      </div>
+    </>
   );
 }
