1e1620a7388f26b9bcc8f6b0d728ad5d33e85e02
Elsa <eperelli@mitre.org>
2022-09-09 12:39:47 -0400

    Edited function comment

--- util/MeasureCalculation.ts
+++ util/MeasureCalculation.ts
@@ -5,12 +5,6 @@ import { createPatientBundle } from './fhir';
 /**
  * Takes in a patient's test case info, a measure bundle, and a measurement
  * period start and end and returns a measureReport
- * passed in filename and fileContents
- * @param patientTestCase {TestCaseInfo}
- * @param mb {fhir4.Bundle}
- * @param mpStart {string}
- * @param mpEnd {string}
- * @returns MeasureReport
  */
 export async function calculateMeasureReport(
   patientTestCase: TestCaseInfo,
