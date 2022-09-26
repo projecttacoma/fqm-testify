d4fdeec74acd7d3995d96944498a8ef1db65016e
Elsa <eperelli@mitre.org>
2022-09-26 12:21:24 -0400

    ui bug fix

--- components/utils/PatientInfoCard.tsx
+++ components/utils/PatientInfoCard.tsx
@@ -1,6 +1,7 @@
-import { Button, Center, Divider, Grid, Paper, Sx, Text, Tooltip } from '@mantine/core';
+import { Button, Center, Divider, Grid, Group, Menu, Paper, Sx, Text, Tooltip } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 import React from 'react';
-import { Copy, Download, Edit, Trash } from 'tabler-icons-react';
+import { Copy, Dots, Download, Edit, Trash } from 'tabler-icons-react';
 import { getPatientDOBString, getPatientNameString } from '../../util/fhir';
 import PopulationMultiSelect from './PopulationMultiSelect';
 
@@ -21,6 +22,7 @@ export default function PatientInfoCard({
   onDeleteClick,
   selected
 }: PatientInfoCardProps) {
+  const matches = useMediaQuery('(min-width: 1600px)');
   return (
     <Paper
       shadow="sm"
@@ -43,8 +45,8 @@ export default function PatientInfoCard({
         return style;
       }}
     >
-      <Grid justify="center" align="center">
-        <Grid.Col span={5}>
+      <Grid align="center">
+        <Grid.Col span={6}>
           <div>
             <Text size="sm">{getPatientNameString(patient)}</Text>
             <Text size="xs" color="dimmed">
@@ -52,59 +54,106 @@ export default function PatientInfoCard({
             </Text>
           </div>
         </Grid.Col>
-        <Grid.Col span={5} offset={2} style={{ paddingRight: 25 }}>
-          <Center>
-            <Tooltip label="Export Patient" openDelay={1000}>
-              <Button
-                size="xs"
-                aria-label={'Export Patient'}
-                onClick={() => {
-                  onExportClick();
-                }}
-                variant="subtle"
-              >
-                <Download />
-              </Button>
-            </Tooltip>
-            <Tooltip label="Edit Patient" openDelay={1000}>
-              <Button
-                aria-label={'Edit Patient'}
-                onClick={() => {
-                  onEditClick();
-                }}
-                variant="subtle"
-                size="xs"
-              >
-                <Edit />
-              </Button>
-            </Tooltip>
-            <Tooltip label="Copy Patient" openDelay={1000}>
-              <Button
-                size="xs"
-                aria-label={'Copy Patient'}
-                onClick={() => {
-                  onCopyClick();
-                }}
-                variant="subtle"
-              >
-                <Copy />
-              </Button>
-            </Tooltip>
-            <Divider sx={{ height: '48px' }} size="xs" orientation="vertical" />
-            <Tooltip label="Delete Patient" openDelay={1000}>
-              <Button
-                size="xs"
-                aria-label={'Delete Patient'}
-                onClick={() => {
-                  onDeleteClick();
-                }}
-                color="red"
-                variant="subtle"
-              >
-                <Trash />
-              </Button>
-            </Tooltip>
-          </Center>
+        <Grid.Col span={6}>
+          <Group position="right">
+            {matches ? (
+              <Center>
+                <Tooltip label="Export Patient" openDelay={1000}>
+                  <Button
+                    size="xs"
+                    aria-label={'Export Patient'}
+                    onClick={() => {
+                      onExportClick();
+                    }}
+                    variant="subtle"
+                  >
+                    <Download />
+                  </Button>
+                </Tooltip>
+                <Tooltip label="Edit Patient" openDelay={1000}>
+                  <Button
+                    aria-label={'Edit Patient'}
+                    onClick={() => {
+                      onEditClick();
+                    }}
+                    variant="subtle"
+                    size="xs"
+                  >
+                    <Edit />
+                  </Button>
+                </Tooltip>
+                <Tooltip label="Copy Patient" openDelay={1000}>
+                  <Button
+                    size="xs"
+                    aria-label={'Copy Patient'}
+                    onClick={() => {
+                      onCopyClick();
+                    }}
+                    variant="subtle"
+                  >
+                    <Copy />
+                  </Button>
+                </Tooltip>
+                <Divider sx={{ height: '48px' }} size="xs" orientation="vertical" />
+                <Tooltip label="Delete Patient" openDelay={1000}>
+                  <Button
+                    size="xs"
+                    aria-label={'Delete Patient'}
+                    onClick={() => {
+                      onDeleteClick();
+                    }}
+                    color="red"
+                    variant="subtle"
+                  >
+                    <Trash />
+                  </Button>
+                </Tooltip>
+              </Center>
+            ) : (
+              <Menu shadow="md" width={200}>
+                <Menu.Target>
+                  <Button variant="subtle">
+                    <Dots />
+                  </Button>
+                </Menu.Target>
+                <Menu.Dropdown>
+                  <Menu.Label>Patient Actions</Menu.Label>
+                  <Menu.Item
+                    icon={<Download size={14} />}
+                    onClick={() => {
+                      onExportClick();
+                    }}
+                  >
+                    Download Patient
+                  </Menu.Item>
+                  <Menu.Item
+                    icon={<Edit size={14} />}
+                    onClick={() => {
+                      onEditClick();
+                    }}
+                  >
+                    Edit Patient
+                  </Menu.Item>
+                  <Menu.Item
+                    icon={<Copy size={14} />}
+                    onClick={() => {
+                      onCopyClick();
+                    }}
+                  >
+                    Copy Patient
+                  </Menu.Item>
+                  <Menu.Item
+                    icon={<Trash size={14} />}
+                    onClick={() => {
+                      onDeleteClick();
+                    }}
+                  >
+                    Delete Patient
+                  </Menu.Item>
+                </Menu.Dropdown>
+              </Menu>
+            )}
+          </Group>
         </Grid.Col>
       </Grid>
       {selected && <PopulationMultiSelect />}
