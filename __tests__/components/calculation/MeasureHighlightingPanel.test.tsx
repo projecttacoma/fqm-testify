a157ced9b25e7abd3d6ddca54aad2535792ce477
Elsa <eperelli@mitre.org>
2022-10-12 08:48:34 -0400

    Population comparison table

---
+++ __tests__/components/calculation/MeasureHighlightingPanel.test.tsx
@@ -0,0 +1,210 @@
+import '@testing-library/jest-dom';
+import { act, render, screen } from '@testing-library/react';
+import { MeasureReport } from 'fhir/r4';
+import { Suspense } from 'react';
+import MeasureHighlightingPanel from '../../../components/calculation/MeasureHighlightingPanel';
+import { measureBundleState } from '../../../state/atoms/measureBundle';
+import { measureReportLookupState } from '../../../state/atoms/measureReportLookup';
+import { patientTestCaseState, TestCase } from '../../../state/atoms/patientTestCase';
+import { selectedPatientState } from '../../../state/atoms/selectedPatient';
+import { getMockRecoilState, mantineRecoilWrap } from '../../helpers/testHelpers';
+
+const MOCK_MEASURE_REPORT: fhir4.MeasureReport = {
+  resourceType: 'MeasureReport',
+  type: 'individual',
+  status: 'complete',
+  measure: '',
+  period: {
+    start: '',
+    end: ''
+  },
+  group: [
+    {
+      id: '',
+      population: [
+        {
+          count: 1,
+          code: {
+            coding: [
+              {
+                system: '',
+                code: 'initial-population',
+                display: 'Initial Population'
+              }
+            ]
+          }
+        },
+        {
+          count: 1,
+          code: {
+            coding: [
+              {
+                system: '',
+                code: 'numerator',
+                display: 'Numerator'
+              }
+            ]
+          }
+        },
+        {
+          count: 1,
+          code: {
+            coding: [
+              {
+                system: '',
+                code: 'denominator-exclusion',
+                display: 'Denominator Exclusion'
+              }
+            ]
+          }
+        },
+        {
+          count: 0,
+          code: {
+            coding: [
+              {
+                system: '',
+                code: 'denominator',
+                display: 'Denominator'
+              }
+            ]
+          }
+        }
+      ]
+    }
+  ]
+};
+
+const TEST_MEASURE_BUNDLE: fhir4.Bundle = {
+  resourceType: 'Bundle',
+  type: 'transaction',
+  entry: [
+    {
+      resource: {
+        resourceType: 'Measure',
+        status: 'active',
+        group: [
+          {
+            population: [
+              {
+                code: {
+                  coding: [
+                    {
+                      system: 'test-system',
+                      code: 'denominator',
+                      display: 'Denominator'
+                    }
+                  ]
+                },
+                criteria: {
+                  language: 'text/cql.identifier',
+                  expression: 'Denominator'
+                }
+              },
+              {
+                code: {
+                  coding: [
+                    {
+                      system: 'test-system',
+                      code: 'numerator',
+                      display: 'Numerator'
+                    }
+                  ]
+                },
+                criteria: {
+                  language: 'text/cql.identifier',
+                  expression: 'Numerator'
+                }
+              },
+              {
+                code: {
+                  coding: [
+                    {
+                      system: 'test-system',
+                      code: 'initial-population',
+                      display: 'Initial Population'
+                    }
+                  ]
+                },
+                criteria: {
+                  language: 'text/cql.identifier',
+                  expression: 'Initial Population'
+                }
+              },
+              {
+                code: {
+                  coding: [
+                    {
+                      system: 'test-system',
+                      code: 'denominator-exclusion',
+                      display: 'Denominator Exclusion'
+                    }
+                  ]
+                },
+                criteria: {
+                  language: 'text/cql.identifier',
+                  expression: 'Denominator Exclusion'
+                }
+              }
+            ]
+          }
+        ]
+      }
+    }
+  ]
+};
+
+const MEASURE_BUNDLE_POPULATED = {
+  name: 'measureBundle',
+  content: TEST_MEASURE_BUNDLE
+};
+
+const MOCK_MEASURE_REPORT_LOOKUP: Record<string, MeasureReport> = {
+  'example-pt': MOCK_MEASURE_REPORT
+};
+
+const EXAMPLE_PATIENT: fhir4.Patient = {
+  resourceType: 'Patient',
+  id: 'example-pt',
+  name: [{ given: ['Test123'], family: 'Patient456' }],
+  birthDate: '1996-07-19'
+};
+
+const MOCK_TEST_CASE: TestCase = {
+  'example-pt': {
+    patient: EXAMPLE_PATIENT,
+    resources: [],
+    desiredPopulations: ['initial-population']
+  }
+};
+
+describe('MeasureHighlightingPanel', () => {
+  it('should render population comparison table', async () => {
+    const MockMB = getMockRecoilState(measureBundleState, MEASURE_BUNDLE_POPULATED);
+    const MockPatients = getMockRecoilState(patientTestCaseState, MOCK_TEST_CASE);
+    const MockSelectedPatient = getMockRecoilState(selectedPatientState, 'example-pt');
+    const MockMeasureReportLookup = getMockRecoilState(measureReportLookupState, MOCK_MEASURE_REPORT_LOOKUP);
+
+    await act(async () => {
+      render(
+        mantineRecoilWrap(
+          <>
+            <MockMB />
+            <MockPatients />
+            <MockSelectedPatient />
+            <MockMeasureReportLookup />
+            <Suspense>
+              <MeasureHighlightingPanel patientId={'example-pt'} />
+            </Suspense>
+          </>
+        )
+      );
+    });
+
+    const numQualified = screen.getAllByText('1');
+    const numUnqualified = screen.getAllByText('0');
+
+    expect(numQualified.length).toBe(4);
+    expect(numUnqualified.length).toBe(4);
+  });
+});
---
+++ components/calculation/MeasurePopulations.tsx
@@ -0,0 +1,45 @@
+import { Enums } from 'fqm-execution';
+
+export interface MultiSelectData {
+  value: string;
+  label: string;
+  disabled: boolean;
+}
+
+/**
+ * Compiles an array of data for the MultiSelect component, using population codes
+ * from the measure resource, across all groups in the measure.
+ *
+ * Excludes measure-population, measure-population-exclusion, and measure-observation
+ * (used for CV measures).
+ * @param {fhir4.Measure} measure - FHIR measure resource
+ * @returns {Array} array of strings representing all unique measure populations
+ */
+export function getMeasurePopulations(measure: fhir4.Measure): MultiSelectData[] {
+  const measurePopulations: MultiSelectData[] = [];
+  const EXCLUDED_MEASURE_POPULATIONS = [
+    Enums.PopulationType.MSRPOPL,
+    Enums.PopulationType.MSRPOPLEX,
+    Enums.PopulationType.OBSERV
+  ];
+  // Iterate over measure population groups
+  measure.group?.forEach(group => {
+    group.population?.forEach(population => {
+      const populationCode = population.code?.coding?.[0].code;
+      const populationDisplay = population.code?.coding?.[0].display;
+      // TODO: determine handling of populations that are not permitted for proportion measures
+      if (
+        populationCode &&
+        !measurePopulations.map(p => p.value).includes(populationCode) &&
+        !EXCLUDED_MEASURE_POPULATIONS.find(p => p === populationCode)
+      ) {
+        measurePopulations.push({
+          value: populationCode,
+          label: populationDisplay || populationCode,
+          disabled: false
+        });
+      }
+    });
+  });
+  return measurePopulations;
+}
