50d61028e5b9d20a7367888117172c7b317ecc2f
Elsa Perelli <eperelli@mitre.org>
2023-06-07 12:10:42 -0400

    Scrollable panels (#72)
    * Rebase scrollable on collapsible
    
    * Make highlighting area fill remaining space when population table collapses
    
    ---------
    
    Co-authored-by: lmd59 <laurend@mitre.org>

--- components/TestCaseEditor.tsx
+++ components/TestCaseEditor.tsx
@@ -1,5 +1,5 @@
-import { Center, createStyles, Grid, Loader, Stack, Text } from '@mantine/core';
-import React from 'react';
+import { Accordion, Box, Center, createStyles, Grid, Loader, ScrollArea, Space, Text } from '@mantine/core';
+import React, { useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { selectedPatientState } from '../state/atoms/selectedPatient';
 import PatientCreationPanel from './patient-creation/PatientCreationPanel';
@@ -9,17 +9,21 @@ import { patientTestCaseState } from '../state/atoms/patientTestCase';
 import { calculationLoading } from '../state/atoms/calculationLoading';
 import { CircleCheck } from 'tabler-icons-react';
 import { getPatientNameString } from '../util/fhir/patient';
+import PopulationComparisonTable from './calculation/PopulationComparisonTable';
+import PopulationComparisonTableControl from './calculation/PopulationComparisonTableControl';
 
 const useStyles = createStyles({
   panel: {
     maxHeight: '100%',
     overflow: 'scroll'
   },
-  header: {
-    maxHeight: '100%'
+  calculation: {
+    maxHeight: '100%',
+    display: 'flex',
+    flexDirection: 'column'
   },
   highlighting: {
-    maxHeight: 'calc(100% - 50px)',
+    flex: 1,
     overflow: 'scroll'
   }
 });
@@ -53,6 +57,7 @@ export default function TestCaseEditor() {
   const selectedPatient = useRecoilValue(selectedPatientState);
   const currentPatients = useRecoilValue(patientTestCaseState);
   const { classes } = useStyles();
+  const [accValue, setAccValue] = useState<string | null>('table');
 
   const renderPanelPlaceholderText = () => {
     return (
@@ -79,8 +84,8 @@ export default function TestCaseEditor() {
         >
           <ResourcePanel />
         </Grid.Col>
-        <Grid.Col span={6} className={classes.header} sx={theme => ({ backgroundColor: theme.colors.gray[1] })}>
-          <Stack style={{ height: 50 }}>
+        <Grid.Col span={6} className={classes.calculation} sx={theme => ({ backgroundColor: theme.colors.gray[1] })}>
+          <Box h={50}>
             {selectedPatient ? (
               <Grid justify="space-between">
                 <Grid.Col span={4}>
@@ -91,10 +96,27 @@ export default function TestCaseEditor() {
             ) : (
               renderPanelPlaceholderText()
             )}
-          </Stack>
-          <Stack className={classes.highlighting}>
+          </Box>
+          <Space />
+          <ScrollArea style={accValue ? { flex: 1 } : {}}>
+            {selectedPatient ? (
+              <Accordion chevronPosition="left" defaultValue="table" value={accValue} onChange={setAccValue}>
+                <Accordion.Item value="table">
+                  <Accordion.Control>
+                    <PopulationComparisonTableControl />
+                  </Accordion.Control>
+                  <Accordion.Panel>
+                    <PopulationComparisonTable patientId={selectedPatient} />
+                  </Accordion.Panel>
+                </Accordion.Item>
+              </Accordion>
+            ) : (
+              ''
+            )}
+          </ScrollArea>
+          <ScrollArea className={classes.highlighting}>
             {selectedPatient ? <MeasureHighlightingPanel patientId={selectedPatient} /> : ''}
-          </Stack>
+          </ScrollArea>
         </Grid.Col>
       </Grid>
     </>
